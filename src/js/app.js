d60eed20ac6fa1f1c51b3bf3a833ce406b067501
TheCing <jptyndalljr@gmail.com>
2018-10-10 12:57:19 -0400

    Add jQuery, navigation

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,51 @@
 // JS Goes here - ES6 supported
+import $ from 'jquery';
+import Color from 'jquery-color';
+window.jQuery = $;
+window.$ = $;
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/js/app.js");
+
+$(function(){
+  console.log("jquery is ready!");
+});
+
+$("body").on('click', '#bars', function() {
+  if($("#sidenav").css("width")!= "250px") {
+    openNav();
+  }
+  else {
+    closeNav();
+  }
+});
+
+function openNav() {
+    $("#sidenav").animate({
+      width: '250px'
+    }, { duration: 200, queue: false });
+    $("html").animate({
+      marginLeft: '250px'
+    }, { duration: 200, queue: false });
+    $(".main").animate({
+      backgroundColor: 'rgba(0,0,0,0.6)'
+    }, { duration: 200, queue: false });
+    window.addEventListener('scroll', noscroll);
+}
+
+function closeNav() {
+  $("#sidenav").animate({
+    width: '0px'
+  }, { duration: 0.5, queue: false });
+  $("html").animate({
+    marginLeft: '0px'
+  }, { duration: 350, queue: false });
+  $(".main").animate({
+    backgroundColor: 'rgba(0,0,0,0)'
+  }, { duration: 400, queue: false });
+  window.removeEventListener('scroll', noscroll);
+}
+
+function noscroll() {
+  window.scrollTo( 0, 0 );
+}
